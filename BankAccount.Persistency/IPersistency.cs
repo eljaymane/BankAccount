381a178f3f02ebc91234ec647d7a106836d21c19
El Jahrani Aymane <aymane.el-jahrani@etu.univ-orleans.fr>
2022-05-19 20:44:04 +0200

    Made Persistence module more generic for future re-use

---
+++ BankAccount.Persistency/IPersistency.cs
@@ -0,0 +1,18 @@
+﻿using BankAccount.Persistency.Adapters.Adapter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount.Persistency
+{
+    public interface IPersistency<T1, T2, ID>
+    {
+        ID addObject(T1 adapter, ID id);
+        T1 getObjectById(ID id);
+        T1 updateObject(ID id, T1 adapter);
+        T2 getObjects();
+        T1 deleteObject(ID id);
+    }
+}
